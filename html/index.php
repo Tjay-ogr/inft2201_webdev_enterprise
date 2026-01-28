e2260b8cc7cc18ab320958db8dbf1ab9b95804c1
Tjay-ogr <gumburatichafara@gmail.com>
2026-01-28 13:50:54 -0500

    1st demo

---
+++ html/index.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+echo "<h1>SupportDesk Demo</h1>";
+echo "<p>PHP + Apache container is running!</p>";
+echo "<p>Next step: add REST endpoints + Composer + PHP.</p>";
