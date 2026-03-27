e8a72f08f19e662806381f9a583d8f641c8d34ba
Tjay Gumbura <tichafara.gumbura@dcmail.ca>
2026-03-27 09:22:34 -0400

    Fix API structure and implement JWT authorization cleanly

--- assignment2/codebase/html/api/mail/index.php
+++ assignment2/codebase/html/api/mail/index.php
@@ -9,7 +9,31 @@ use Application\Verifier;
 $database = new Database('prod');
 $page = new Page();
 
+
 $mail = new Mail($database->getDb());
+
+$mail = new Mail($database->getDb());
+
+// verify JWT and get user info
+$verifier = new Verifier();
+$user = $verifier->verifyToken();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // only allow valid roles
+    if ($user->role !== 'user' && $user->role !== 'admin') {
+        http_response_code(403);
+        echo json_encode(["error" => "Forbidden"]);
+        exit;
+    }
+
+     $data = json_decode(file_get_contents('php://input'), true);
+        if (array_key_exists('name', $data) && array_key_exists('message', $data)) {
+        $id = $mail->createMail($data['name'], $data['message']);
+        $page->item(array("id" => $id));
+    } else {
+        $page->badRequest();
+    }
 
 $verifier = new Verifier();
 $verifier->decode($_SERVER['HTTP_AUTHORIZATION']);
