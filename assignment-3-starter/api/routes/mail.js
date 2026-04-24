4722160ffe5f86895c2f89e48ffd957df81dc6a8
Tjay Gumbura <tichafara.gumbura@dcmail.ca>
2026-04-24 18:29:25 -0400

    Fix mail route and implement proper RBAC flow

--- assignment-3-starter/api/routes/mail.js
+++ assignment-3-starter/api/routes/mail.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mailData = require("../data/mail");
+
 const authenticateJWT = require("../middleware/authenticateJWT");
 const authorize = require("../middleware/authorize");
 const canViewMail = require("../policies/canViewMail");
@@ -12,8 +13,11 @@ function loadMail(req, res, next) {
   const mail = mailData.find(m => m.id === id);
 
   if (!mail) {
-    // TODO: create a "not found" error object and pass to next(err)
-    return next(new Error("Mail not found (placeholder error, customize me)"));
+    return next({
+      statusCode: 404,
+      error: "NotFound",
+      message: "Mail not found"
+    });
   }
 
   req.mail = mail;
@@ -21,15 +25,11 @@ function loadMail(req, res, next) {
 }
 
 // GET /mail/:id
-// Requirements:
-// - Must be authenticated (JWT)
-// - Must satisfy canViewMail policy (admin OR owner)
 router.get("/:id",
   authenticateJWT,
   loadMail,
   authorize(canViewMail),
   (req, res) => {
-    // At this point, user is authenticated and authorized.
     res.json(req.mail);
   }
 );
